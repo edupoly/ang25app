d31216e0de119e8d89745e2cff5a51318add4e5a
edupoly <info.edupoly@gmail.com>
2025-08-13 19:39:32 +0530

    forloop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,62 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ang25app';
+  students = [
+    'sachin',
+    'rohit',
+    'kohli',
+    'samanta',
+    'anushka',
+    'kumar sangakkara',
+    'mahela jayawardene',
+    'lasith malinga',
+    'angelo mathews',
+    'tm dilshan',
+  ];
+  employees = [
+    {
+      firstname: 'salman',
+      id: 1,
+      bestpair: 'katrina',
+    },
+    {
+      firstname: 'Amir',
+      id: 2,
+      bestpair: 'karishma',
+    },
+    {
+      firstname: 'Akshay',
+      id: 3,
+      bestpair: 'sonakshi',
+    },
+    {
+      firstname: 'Saif',
+      id: 4,
+      bestpair: 'kareena',
+    },
+    {
+      firstname: 'Anilkapoor',
+      id: 5,
+      bestpair: 'madhuri',
+    },
+    {
+      firstname: 'Ranbir',
+      id: 6,
+      bestpair: 'Deepika',
+    },
+    {
+      firstname: 'Ranveer',
+      id: 7,
+      bestpair: 'Anushka',
+    },
+  ];
 }
