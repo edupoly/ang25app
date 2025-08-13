30211bb19dbf8960718b0e4111d1398ca9318739
edupoly <info.edupoly@gmail.com>
2025-08-13 19:09:13 +0530

    databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ang25app';
+  myname = 'Praveen';
+  gender = null;
+  techs = null;
+  c = false;
+  greeting() {
+    alert('Hello Hyderabad');
+  }
 }
