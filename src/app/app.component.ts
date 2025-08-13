027be0ee13cab7e9e8ca1c8983fb25d8ec7f16b7
edupoly <info.edupoly@gmail.com>
2025-08-13 20:02:04 +0530

    ifelse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ang25app';
+  num: any;
+  gender = null;
+  maleimage =
+    'https://img.freepik.com/premium-photo/generative-ai-male-cyborg-with-artificial-intelligence-concept-dark-background_634053-2773.jpg';
+  femaleimage =
+    'https://img.freepik.com/premium-photo/woman-with-ai-network-her-head-generated-by-ai_793210-2393.jpg?w=360';
 }
