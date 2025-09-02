b0f280e34e52785ec44c47acbcd12c06ddc9b802
edupoly <info.edupoly@gmail.com>
2025-09-02 17:30:11 +0530

    datatables-with-pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { products } from './products.json';
+import { JsonPipe, SlicePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, RouterOutlet, JsonPipe, SlicePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ang25app';
+  allproducts = signal(products);
+  si = signal(0);
+  perpage = signal(5);
+  nextPage() {
+    this.si.update((cv) => {
+      return cv + this.perpage();
+    });
+  }
+  prevPage() {
+    this.si.update((cv) => {
+      return cv - this.perpage();
+    });
+  }
+  updatePerPage(ev: any) {
+    this.perpage.set(+ev.target.value);
+  }
 }
