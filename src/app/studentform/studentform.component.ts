c8d0a1eb0b5eee424d70ee8e0b6dcf58261a5a62
edupoly <info.edupoly@gmail.com>
2025-10-01 11:39:24 +0530

    practice4

---
+++ src/app/studentform/studentform.component.ts
@@ -0,0 +1,38 @@
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-studentform',
+  imports: [ReactiveFormsModule, NgIf],
+  templateUrl: './studentform.component.html',
+  styleUrl: './studentform.component.css',
+})
+export class StudentformComponent {
+  studentForm!: FormGroup;
+  constructor(public fb: FormBuilder) {
+    this.studentForm = this.fb.group({
+      firstname: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      lastname: this.fb.control([]),
+      age: this.fb.control([]),
+    });
+  }
+  show() {
+    console.log(this.studentForm);
+    console.log(this.studentForm.value);
+  }
+  loadData() {
+    this.studentForm.patchValue({
+      firstname: 'ravi',
+      lastname: 'hhais',
+    });
+  }
+}
