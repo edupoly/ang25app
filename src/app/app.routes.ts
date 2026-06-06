f46d6cef1ed372f4d8316cd93488859f17bb2a6a
edupoly <info.edupoly@gmail.com>
2026-06-06 19:48:25 +0530

    routeguards-todoapp-auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TodosComponent } from './todos/todos.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,6 @@ export const routes: Routes = [
   {
     path: 'todos',
     component: TodosComponent,
+    canActivate: [authGuard],
   },
 ];
