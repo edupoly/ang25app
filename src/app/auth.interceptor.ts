b06675c20cafae1adffb4e9e37247afc9bd26aab
edupoly <info.edupoly@gmail.com>
2026-06-06 19:44:50 +0530

    routeguards-todoapp-auth

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,7 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log('interception call indiroy...');
   let clonedReq = req.clone({
     setHeaders: {
       testheader: 'Test kosam',
